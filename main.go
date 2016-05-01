6ace38d5e71b2b3af53d97932be90e9c9e11da8e
Soheil Eizadi <seizadi@infoblox.com>
2016-05-01 16:14:17 -0700

    First commit

--- main.go
+++ main.go
@@ -2,7 +2,26 @@ package main
 
 import "fmt"
 
+
 func main() {
-	fmt.Println("Hello World!")
-	fmt.Println("Hello Soheil!")
+
+	c := make(chan int)
+	e := make(chan bool)
+	// Producer
+	go func() {
+		defer close(c)
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+	}()
+
+	// Consumer
+	go func() {
+		for i:= range c {
+			fmt.Println(i)
+		}
+		e <- true
+	}()
+
+	<-e
 }
